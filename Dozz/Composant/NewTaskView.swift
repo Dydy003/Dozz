dbdd9d6b73a451bba1401e51751f1b9a9385b4f7
Dydy003 <dcaetano8@icloud.com>
2024-07-05 01:28:57 +0200

    1.2

---
+++ Dozz/Composant/NewTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  NewTaskView.swift
+//  Dozz
+//
+//  Created by Dylan Caetano on 05/07/2024.
+//
+
+import SwiftUI
+
+struct NewTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewTaskView()
+}
