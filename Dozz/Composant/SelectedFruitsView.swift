f31b12c756c724748b4d05437718b37a985d5b60
Dydy003 <dcaetano8@icloud.com>
2024-07-06 01:29:09 +0200

    ListReview

---
+++ Dozz/Composant/SelectedFruitsView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectedFruitsView.swift
+//  Dozz
+//
+//  Created by Dylan Caetano on 05/07/2024.
+//
+
+import SwiftUI
+
+struct SelectedFruitsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectedFruitsView()
+}
