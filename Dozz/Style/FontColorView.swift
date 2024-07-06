9ebbd38a419a5000f80432251361d5bb652d9ee8
Dydy003 <dcaetano8@icloud.com>
2024-07-06 21:18:42 +0200

    Selected

---
+++ Dozz/Style/FontColorView.swift
@@ -0,0 +1,18 @@
+//
+//  FontColorView.swift
+//  Dozz
+//
+//  Created by Dylan Caetano on 06/07/2024.
+//
+
+import SwiftUI
+
+struct FontColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FontColorView()
+}
